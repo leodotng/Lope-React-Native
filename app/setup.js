0f5807fb3097b90c08cfc6e89b77dfef6cc46cd8
KELSEY LAVER <KELSEYLAVER@GMAIL.COM>
2018-01-05 14:01:27 -0700

    Setup.js updated

--- app/setup.js
+++ app/setup.js
@@ -1,15 +1,17 @@
+'use strict';
+
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
 import store from '../app/store'; //import the store
-import Home from '../app/components/home' //Import the component file
+import Main from '../app/index' //Import the component file
 
 function setup() {
   class Root extends Component {
     render () {
       return (
         <Provider store={store}>
-          <Home />
+          <Main />
         </Provider>
       );
     }
