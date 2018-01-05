67e9b6ad49e50f4e0f4354c31883dfdcd522537d
KELSEY LAVER <KELSEYLAVER@GMAIL.COM>
2018-01-05 12:57:54 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+//
+// AppRegistry.registerComponent('LopeNative', () => App);
 
-AppRegistry.registerComponent('LopeNative', () => App);
+// Update index.ios.js and index.android.js
+import { AppRegistry } from 'react-native';
+import setup from './app/setup';
+
+AppRegistry.registerComponent('ReactReduxBoilerPlate', () => setup());
