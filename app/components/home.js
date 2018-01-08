04105ce6fc25244ceb5b1db8b3bb3db8427cc418
KELSEY LAVER <KELSEYLAVER@GMAIL.COM>
2018-01-08 13:58:48 -0700

    Maybe CRUD?

--- app/components/home.js
+++ app/components/home.js
@@ -1,113 +1,168 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 var {
-  Stylesheet,
-  ListView,
-  View,
-  Text,
-  ActivityIndicator
+    StyleSheet,
+    ListView,
+    View,
+    Text,
+    ActivityIndicator, TouchableHighlight, ActionSheetIOS, TouchableWithoutFeedback
 } = require('react-native');
 
 import {bindActionCreators} from 'redux';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
-import * as Actions from '../actions'; //import your actions
+import * as ReduxActions from '../actions'; //Import your actions
+
+import {Actions} from 'react-native-router-flux';
+
+
+//Buttons for Action Sheet
+var BUTTONS = [
+    "Edit",
+    "Delete",
+    'Cancel',
+];
+
+var CANCEL_INDEX = 2;
+
+var _this;
+
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      ds: ds
-    };
-  }
+        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            ds: ds
+        };
+    }
 
-  componentDidMount() {
-    this.props.getData(); //call our action
-  }
+    componentDidMount() {
+        this.props.getQuotes();
+        _this = this;
+    }
 
-  render() {
-    if (this.props.loading) {
-      return (
-        <View style={styles.activityIndicatorContainer}>
-          <ActivityIndicator
-            animating={true}
-            style={[{height: 80}]}
-            size="small"
-          />
-        </View>
-      );
-    } else {
-      return (
-        <View style={{flex:1, backgroundColor: '#f5f5f5', paddingTop:20}}>
-          <ListView enableEmptySections={true}
-            dataSource={this.state.ds.cloneWithRows(this.props.data)}
-            renderRow={this.renderRow.bind(this)}/>
-        </View>
-      );
-  }
-}
+    render() {
+        if (this.props.loading) {
+            return (
+                <View style={styles.activityIndicatorContainer}>
+                    <ActivityIndicator
+                        animating={true}
+                        style={[{height: 80}]}
+                        size="small"
+                    />
+                </View>
+            );
+        } else {
+            return (
+                <View style={{flex: 1, backgroundColor: '#eaeaea'}}>
+                    <ListView enableEmptySections={true}
+                              dataSource={this.state.ds.cloneWithRows(this.props.quotes)}
+                              renderRow={this.renderRow.bind(this)}/>
 
-renderRow(rowData, sectionID, rowID) {
-  return (
-    <View style={styles.row}>
-      <Text style={styles.title}>
-        {(parseInt(rowID) + 1)}{". "}{rowData.title}
-      </Text>
-      <Text style={styles.description}>
-        {rowData.decription}
-      </Text>
-    </View>
-    )
-  }
+                    <TouchableHighlight style={styles.addButton}
+                                        underlayColor='#ff7043' onPress={() => Actions.new_quote()}>
+                        <Text style={{fontSize: 25, color: 'white'}}>+</Text>
+                    </TouchableHighlight>
+                </View>
+            );
+        }
+    }
+
+    renderRow(rowData, sectionID, rowID) {
+        return (
+            <TouchableWithoutFeedback onPress={() => this.showOptions(rowData)}>
+                <View style={styles.row}>
+                    <Text style={styles.description}>
+                        {rowData.quote}
+                    </Text>
+                    <Text style={styles.author}>
+                        {rowData.author}
+                    </Text>
+                </View>
+            </TouchableWithoutFeedback>
+        )
+    }
+
+    showOptions(quote) {
+        ActionSheetIOS.showActionSheetWithOptions({
+                options: BUTTONS,
+                cancelButtonIndex: CANCEL_INDEX,
+                destructiveButtonIndex: 1,
+            },
+            (buttonIndex) => {
+                if (buttonIndex === 0) Actions.new_quote({quote: quote, edit: true, title:"Edit Quote"})
+                else if (buttonIndex === 1) _this.props.deleteQuote(quote.id)
+            });
+    }
 };
 
+
 // The function takes data from the app current state,
-// and insert/links it to the props of our component
+// and insert/links it into the props of our component.
 // This function makes Redux know that this component needs to be passed a piece of the state
-
 function mapStateToProps(state, props) {
-  return {
-    loading: state.dataReducer.loading,
-    data: state.dataReducer.data
-  }
+    return {
+        loading: state.dataReducer.loading,
+        quotes: state.dataReducer.quotes
+    }
 }
 
-// Doing this merges our actions into the components props,
-// while wrapping them in dispatch() so that they immediately dispatch in Action
-// Just by doing this, we will have access to the actions defined in our actions file (action/home.js)
-
+// Doing this merges our actions into the component’s props,
+// while wrapping them in dispatch() so that they immediately dispatch an Action.
+// Just by doing this, we will have access to the actions defined in out actions file (action/home.js)
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(Actions, dispatch);
+    return bindActionCreators(ReduxActions, dispatch);
 }
 
-// Connect everything
+//Connect everything
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
 
 var styles = StyleSheet.create({
-  activityIndicatorContainer:{
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1
-  },
+    activityIndicatorContainer: {
+        backgroundColor: "#fff",
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1
+    },
 
-  row:{
-    borderBottomWidth: 1,
-    borderColor: "ccc",
-    // height: 50,
-    padding: 10
-  },
+    row: {
+        backgroundColor: "#fff",
+        padding: 8 * 2,
+        marginBottom: 1
+    },
 
-  title:{
-    fontSize: 15,
-    fontWeight: "600"
-  },
+    author: {
+        fontSize: 14,
+        fontWeight: "600",
+        marginTop: 8 * 2
+    },
 
-  description:{
-    marginTop: 5,
-    fontSize: 14,
-  }
+    quote: {
+        marginTop: 5,
+        fontSize: 14,
+    },
+
+    addButton: {
+        backgroundColor: '#ff5722',
+        borderColor: '#ff5722',
+        borderWidth: 1,
+        height: 50,
+        width: 50,
+        borderRadius: 50 / 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+            height: 1,
+            width: 0
+        }
+    }
 });
